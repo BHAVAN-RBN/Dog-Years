5975f9cc93749a4b18e078cdeda8844d039721b3
BHAVAN-RBN <88610966+BHAVAN-RBN@users.noreply.github.com>
2023-07-01 16:37:55 +0530

    Add files via upload

---
+++ dog years calculater.js
@@ -0,0 +1,9 @@
+const myAge =3
+let earlyYears=2
+earlyYears*=10.5
+let laterYear=myAge-2
+laterYear*=4
+console.log(`The earlyYear is ${earlyYears}.`+`the lateryear is ${laterYear}.`)
+let myAgeInDogYears=earlyYears+laterYear
+let myName = 'hop'.toLowerCase()
+console.log(`My name is ${myName}.I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
